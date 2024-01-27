1f552cbbcc9d02772f1d1700ccad99a765fe06dd
tsukaryu <drgon8otsuka@gmail.com>
2024-01-27 23:34:10 +0900

    test: vector動いた

--- a/Main.hs
+++ a/Main.hs
@@ -1,4 +1,14 @@
-import HaskellSay (haskellSay)
+import Data.Vector.Unboxed qualified as U
 
 main :: IO ()
-main = haskellSay "a!"
+main = do
+  let x = digits10'U 12345
+  print x
+
+digits10'U :: Int -> U.Vector Int
+digits10'U = U.unfoldr f
+  where
+    f 0 = Nothing
+    f x = Just (r, q)
+      where
+        (q, r) = x `divMod` 10
