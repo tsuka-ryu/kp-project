981b1fee1b265ecbbd15173f5ae89205a3cb15ed
tsukaryu <drgon8otsuka@gmail.com>
2024-01-30 23:14:45 +0900

    bytestring, extra, arrayを導入、他のバージョンもテンプレに合わせた

---
+++ toyboot/iarray/Main.hs
@@ -0,0 +1,27 @@
+
+
+import Control.Monad
+import Data.Array (Array)
+import Data.Array.IArray
+import Data.Array.Unboxed (UArray)
+import Data.ByteString.Char8 qualified as BS
+import Data.Char
+import Data.List qualified as L
+import Data.List.Extra (chunksOf)
+
+-- Data.UArrayには関数がないので、型クラスIArrayの関数を使用する
+-- Data.Arrayには関数があるが、IArrayの関数名と衝突するので、IArrayの関数を使う
+
+ints :: IO [Int]
+ints = L.unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
+
+ints2 :: IO (Int, Int)
+ints2 = do
+  [x1, x2] <- ints
+  return (x1, x2)
+
+getMatInt :: Int -> Int -> IO (UArray (Int, Int) Int)
+getMatInt h w = listArray ((0, 0), (h - 1, w - 1)) . concat <$> replicateM h ints
+
+-- IArrayの主なAPIは!演算子とaccumArray
+-- 多次元への1点アクセスと、畳み込み
