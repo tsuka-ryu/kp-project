86575a42ce8593d438da6c8d8909e8abb8aac2f6
tsukaryu <drgon8otsuka@gmail.com>
2024-01-31 23:37:15 +0900

    iarray

--- toyboot/iarray/Main.hs
+++ toyboot/iarray/Main.hs
@@ -1,5 +1,4 @@
 import Control.Monad
-import Data.Array (Array)
 import Data.Array.IArray
 import Data.Array.Unboxed (UArray)
 import Data.ByteString.Char8 qualified as BS
@@ -15,21 +14,39 @@ main = do
   [h, w] <- ints
   mat <- getMatInt h w
   -- 配列の添字範囲
-  print $ bounds mat
+  -- print $ bounds mat
   -- 配列中の値の一覧（@listArray@への引数を表示）
-  print $ elems mat
+  -- print $ elems mat
   -- 配列中の（添字、値）の一覧
-  print $ assocs mat
+  -- print $ assocs mat
   -- bounds およびassocsの組み合わせ
-  print mat
+  -- print mat
+  -- 各行の和を求める
+  let rowSums = listArray @UArray (0, h - 1) $ [sum [mat ! (y, x) | x <- [0 .. w - 1]] | y <- [0 .. h - 1]]
+  -- print rowSums
+
+  -- 各列の和を求める
+  let colSums = listArray @UArray (0, w - 1) $ [sum [mat ! (y, x) | y <- [0 .. h - 1]] | x <- [0 .. w - 1]]
+  -- print colSums
+
+  -- 各点における cross sum を求める
+  -- 自分自身は２回カウントされるので、一回除く
+  let res = [rowSums ! y + colSums ! x - mat ! (y, x) | (y, x) <- range (bounds mat)]
+  -- print $ range (bounds mat)
+  print res
+
+  -- 1行ずつ取り出す
+  forM_ (chunksOf w res) $ \row -> do
+    -- 各行を表示する
+    putStrLn $ unwords (map show row)
 
 ints :: IO [Int]
 ints = L.unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
 
-ints2 :: IO (Int, Int)
-ints2 = do
-  [x1, x2] <- ints
-  return (x1, x2)
+-- ints2 :: IO (Int, Int)
+-- ints2 = do
+--   [x1, x2] <- ints
+--   return (x1, x2)
 
 getMatInt :: Int -> Int -> IO (UArray (Int, Int) Int)
 getMatInt h w = listArray ((0, 0), (h - 1, w - 1)) . concat <$> replicateM h ints
