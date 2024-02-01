a5a7d5a553adb0b643e0a359387e57e1b379627d
tsukaryu <drgon8otsuka@gmail.com>
2024-02-01 23:34:31 +0900

    Int からNumに拡張されたらしい？

--- toyboot/m-array/Main.hs
+++ toyboot/m-array/Main.hs
@@ -1,11 +1,21 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE QuantifiedConstraints #-}
+
 import Control.Monad -- `forM_` など
-import Control.Monad.ST -- `ST` モナド
+import Control.Monad.ST (ST) -- `ST` モナド
 import Data.Array (Array)
 import Data.Array.IArray
 import Data.Array.IO -- `IOArray`, `IOUArray` (`MArray` のインスタンス)
 import Data.Array.MArray -- 可変配列の API (型クラス `MArray`)
 import Data.Array.ST -- `STArray`, `STUArray` (`MArray` のインスタンス)
 import Data.Array.Unboxed (UArray)
+
+main :: IO ()
+main = do
+  let csum1 = mutCSum1 3 [1 .. 3]
+  print $ elems csum1
+  let csum2 = mutCSum2 3 [1.0 :: Double .. 3.0]
+  print $ elems csum2
 
 mutCSum1 :: Int -> [Int] -> UArray Int Int
 mutCSum1 n xs = runSTUArray $ do
@@ -16,7 +26,11 @@ mutCSum1 n xs = runSTUArray $ do
     writeArray arr (i + 1) $! acc + dx
   return arr
 
-main :: IO ()
-main = do
-  let csum1 = mutCSum1 3 [1 .. 3]
-  print $ elems csum1
+mutCSum2 :: (Num e, forall s. MArray (STUArray s) e (ST s)) => Int -> [e] -> UArray Int e
+mutCSum2 n xs = runSTUArray $ do
+  arr <- newArray (0, n) 0
+
+  forM_ (zip [0 .. n - 1] xs) $ \(!i, !dx) -> do
+    x <- readArray arr i
+    writeArray arr (i + 1) $! x + dx
+  return arr
