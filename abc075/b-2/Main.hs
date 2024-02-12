8b01dae295bf1a82a7fe877d8ba8fca1454eafa6
tsukaryu <drgon8otsuka@gmail.com>
2024-02-12 19:25:42 +0900

    完全に理解した

--- abc075/b-2/Main.hs
+++ abc075/b-2/Main.hs
@@ -1,20 +1,10 @@
 import Control.Monad
 import Data.Array
-import Debug.Trace (traceShow)
 
 main = do
   [h, w] <- map read . words <$> getLine
   ss <- replicateM h getLine
   let ans = abc075b h w ss
-  print $ tcnts h w ss
-  print $ zip [1 ..] ss
-  print $ [(j, '#') | (j, '#') <- zip [1 ..] "....."]
-  print $ [(j, '#') | (j, '#') <- zip [1 ..] ".#.#."]
-  print $ [(j, '#') | (j, '#') <- zip [1 ..] "....."]
-  -- #の位置が取れる
-  --   []
-  -- [(2,'#'),(4,'#')]
-  -- []
   mapM_ putStrLn ans
 
 abc075b :: Int -> Int -> [String] -> [String]
@@ -39,15 +29,26 @@ abc075b h w ss = ans
       ]
 
 -- 8近傍の#の個数で.を置き換える
+-- この部分は、まず#のある場所を特定している
+-- したのデータの場合、(2,2) (2,4）が#の位置
+-- 3 5
+-- .....
+-- .#.#.
+-- .....
 
-tcnts h w ss =
-  accumArray
-    (+)
-    0
-    ((1, 1), (h, w))
-    [ ((x, y), 1)
-      | (i, s) <- zip [1 ..] ss,
-        (j, '#') <- zip [1 ..] s,
-        x <- [pred i | i > 1] ++ i : [succ i | i < h],
-        y <- [pred j | j > 1] ++ j : [succ j | j < w]
-    ]
+-- #の位置がわかったら、その周囲の値を計算する
+-- (2,2)のとき
+-- x = [1,2,3]
+-- y = [1,2,3]
+-- xとyの非決定性計算で[(1,1),(1,2),(1,3),(2,1),(2,2),(2,3),(3,1),(3,2),(3,3)]
+-- (2,4）のとき
+-- x = [1,2,3]
+-- y = [3,4,5]
+-- xとyの非決定性計算で[(1,3),(1,4),(1,5),(2,3),(2,4),(2,5),(3,3),(3,4),(3,5)]
+-- この結果をたたみ込んで、答えが出る
+-- [ ((x,y),1)
+--       | (i,s) <- zip [1..] ss
+--       , (j,'#') <- zip [1..] s
+--       , x <- [pred i | i > 1] ++ i : [succ i | i < h]
+--       , y <- [pred j | j > 1] ++ j : [succ j | j < w]
+--       ]
