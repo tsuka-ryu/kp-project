d91a862e90170f2db5102d8e60d8b06f11b27e19
tsukaryu <drgon8otsuka@gmail.com>
2024-05-19 00:35:37 +0900

    feat: abc058-c

--- abc058/c/Main.hs
+++ abc058/c/Main.hs
@@ -1,11 +1,17 @@
 import Control.Monad (replicateM)
-import Data.List (foldl1', intersect, sort)
+import Data.Map.Strict qualified as Map
+
+f xs = Map.fromListWith (+) ([(x, 1) | x <- xs] ++ ys)
+  where
+    ys = [(i, 0) | i <- ['a' .. 'z']]
 
 main :: IO ()
 main = do
   n <- readLn :: IO Int
-  ss <- replicateM n  getLine
-  let as = map sort ss
-  let xs = foldl1' intersect as
-  -- print ss
-  putStrLn xs
+  ss <- replicateM n getLine
+  let xs = map f ss
+  let ys = [(i, minimum (map (\x -> x Map.! i) xs)) | i <- ['a' .. 'z']]
+  let ans = concat $ map (\(i, j) -> replicate j i) ys
+  -- print xs
+  -- print ys
+  putStrLn ans 
