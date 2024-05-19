03aa0e44d8c6f4fa65d042014302912cee3246b5
tsukaryu <drgon8otsuka@gmail.com>
2024-05-19 22:18:58 +0900

    feat: past-1-f, 解けなかったのでコードを読んだ

---
+++ past/1/f/Main.hs
@@ -0,0 +1,37 @@
+import Data.Char (isLower, toLower)
+import Data.List qualified as L
+import Debug.Trace (traceShow)
+
+main :: IO ()
+main = do
+  cs <- getLine
+  putStrLn $ solve cs
+
+split :: String -> [String]
+split xs = go xs
+  where
+    go (c : cs) | (xs, y : ys) <- span isLower cs = (c : xs ++ [y]) : go ys
+      where
+        !_ = traceShow c ()
+        !_ = traceShow cs ()
+    go [] = []
+
+solve :: String -> String
+solve = concat . L.sortOn (map toLower) . split
+
+-- splitの処理の流れ
+-- 入力:FisHDoGCaTAAAaAAbCAC
+-- F:isHDoGCaTAAAaAAbCAC
+-- ("is",HDoGCaTAAAaAAbCAC) <- span isLower cs
+-- (F : "is" : "D") : go DoGCaTAAAaAAbCA
+
+-- chatGPT修正版
+-- split :: String -> [String]
+-- split xs = go xs
+--   where
+--     go (c : cs) = 
+--       let (xs, remain) = span isLower cs in
+--       case remain of
+--         (y : ys) -> (c : xs ++ [y]) : go ys
+--         [] -> [c : xs]
+--     go [] = []
