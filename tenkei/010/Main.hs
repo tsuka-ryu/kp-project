795c9d592bb8336066d4037ee6d9dca901853ea4
tsukaryu <drgon8otsuka@gmail.com>
2024-07-20 17:55:47 +0900

    fix: vectorにしたら通った、何？

--- tenkei/010/Main.hs
+++ tenkei/010/Main.hs
@@ -1,8 +1,8 @@
 import Control.Monad (replicateM, replicateM_)
-import Data.Array.Unboxed (UArray, listArray, (!))
 import Data.ByteString.Char8 qualified as BS
 import Data.Char (isSpace)
-import Data.List (unfoldr, scanl')
+import Data.List (unfoldr)
+import Data.Vector.Unboxed qualified as VU
 
 getInts :: IO [Int]
 getInts = unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
@@ -16,13 +16,13 @@ main :: IO ()
 main = do
   [n] <- getInts
   cp <- replicateM n getTuple
-  [q] <- getInts
   let cp' = map (\(c, p) -> if c == 1 then (p, 0) else (0, p)) cp
-  let f = listArray @UArray (1, n + 1) $ scanl' (+) 0 $ map fst cp'
-  let s = listArray @UArray (1, n + 1) $ scanl' (+) 0 $ map snd cp'
+  let f = VU.scanl' (+) 0 $ VU.fromList $ map fst cp'
+  let s = VU.scanl' (+) 0 $ VU.fromList $ map snd cp'
 
+  [q] <- getInts
   replicateM_ q $ do
     [l, r] <- getInts
-    let x1 = f ! (r + 1) - f ! l
-    let x2 = s ! (r + 1) - s ! l
+    let x1 = f VU.! r - f VU.! (l - 1)
+    let x2 = s VU.! r - s VU.! (l - 1)
     putStrLn . unwords . map show $ [x1, x2]
